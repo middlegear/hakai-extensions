5d00f2d037e5f3fc1e81ba91164452dc8eb2efca
BravinJoshua <97509851+BravinJoshua@users.noreply.github.com>
2025-02-22 15:10:47 +0300

    check for dist?

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     name: 'hakai',
-    isolate: false,
-    exclude: ['tests/animez.test.ts'],
     environment: 'node',
     testTimeout: 5000,
   },
