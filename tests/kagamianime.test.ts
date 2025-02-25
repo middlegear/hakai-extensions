7424216a16436433c99ffb06603bc8c59b4a647b
BravinJoshua <97509851+BravinJoshua@users.noreply.github.com>
2025-02-25 21:45:42 +0300

    tests

--- tests/kagamianime.test.ts
+++ tests/kagamianime.test.ts
@@ -2,16 +2,19 @@ import { test, expect } from 'vitest';
 import { KagamiAnime } from '../src/provider/anime/kagamianime';
 import { SubOrDub } from '../src/provider/anime/kagamianime/types';
 const kagamiAnime = new KagamiAnime();
+const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 test('search anime ', async () => {
   const data = await kagamiAnime.search('solo levelling');
-  expect(data.success).toEqual(true);
-  expect(data.data).not.toBe([]);
+  expect(data.success).not.toEqual(false);
+  expect(data.data).not.toEqual([]);
+  await wait(1000);
 });
 
 test('fetch animeinfo including episodes', async () => {
   const data = await kagamiAnime.fetchAnimeInfo('solo-leveling-season-2-arise-from-the-shadow-x7rq');
   expect(data.success).toEqual(true);
   expect(data.data).not.toBe(null);
+  await wait(1000);
 });
 
 test('fetch servers using episodeId', async () => {
@@ -20,6 +23,7 @@ test('fetch servers using episodeId', async () => {
   );
   expect(data.success).toEqual(true);
   expect(data.data).not.toBe([]);
+  await wait(1000);
 });
 
 test('fetch episode sources', async () => {
@@ -29,4 +33,5 @@ test('fetch episode sources', async () => {
   );
   expect(data.success).toEqual(true);
   expect(data.data).not.toBe(null);
+  await wait(1000);
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     name: 'hakai-extensions',
+    isolate: true,
     environment: 'node',
-    testTimeout: 20000, // slow 3g connection lol
+    testTimeout: 20000,
   },
 });
