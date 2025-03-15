39ffe6777a7ebcb7efd959ee8b8d40dd3a662e8a
middle gear <otukojoshua@outlook.com>
2025-03-15 17:09:16 +0000

    export classes

--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,7 @@ import { Anilist, Jikan, Meta } from './provider';
 import { HiAnime } from './provider/anime/hianime';
 import { AnimeKai } from './provider/anime/animekai';
 
-// export { Anime, Meta, Anilist, Jikan, HiAnime, AnimeKai };
-const animekai = new Anime.AnimeKai();
-// const search = await animekai.search('bleach');
-// console.log(search);
-
-// const info = await animekai.fetchAnimeInfo('bleach-thousand-year-blood-war-the-conflict-zev9');
-// console.log(info);
-
-// const servers = await animekai.fetchServers(
-//   'bleach-thousand-year-blood-war-the-conflict-zev9$ep=1$token=MIWzueTktEjhmW9Nj5TU',
-// );
-// console.log(servers);
-
-// const sources = await animekai.fetchSources(
-//   'bleach-thousand-year-blood-war-the-conflict-zev9$ep=1$token=MIWzueTktEjhmW9Nj5TU',
-// );
-// const sources = await animekai.fetchSources('solo-leveling-18718-episode-119497');
-const anilist = new Jikan();
-///5114
-const AnilistEpisodes = await anilist.fetchTopAiring();
-console.log(AnilistEpisodes);
-
+export { Anime, Meta, Anilist, Jikan, HiAnime, AnimeKai };
 import {
   type Seasons,
   type SubOrDub,
