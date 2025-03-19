eb63a4f546d1d33b55171145be61f86ff2c315f6
middle gear <otukojoshua@outlook.com>
2025-03-19 12:55:15 +0300

    another test

--- src/config/headers.ts
+++ src/config/headers.ts
@@ -1,5 +1,3 @@
-export const USER_AGENT_HEADER =
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4692.71 Safari/537.36' as const;
-export const ACCEPT_HEADER =
-  'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8' as const;
+export const USER_AGENT_HEADER = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0' as const;
+export const ACCEPT_HEADER = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' as const;
 export const ACCEPT_ENCODING = 'gzip, deflate, br, zstd' as const;
