f9af95436ba76da57d9a6d3132cc9afa6f8301a5
middle gear <otukojoshua@outlook.com>
2025-07-20 13:55:52 +0300

    chore(delete): delete

--- src/main.ts
+++ src/main.ts
@@ -32,17 +32,4 @@ export {
   StreamingServers,
   AnimeProvider,
 };
-// const data = await _getsources('movie-109831', 'akcloud');
-// const data = await _getsources('episode-1019968', 'vidcloud');
-// const flixhq = new FlixHQ();
-//movie/final-destination-bloodlines-124888'
-// const data = await flixhq.fetchMediaInfo('tv/watch-the-boys-33895');
-// const data = await flixhq.fetchMediaInfo('movie/watch-bad-boys-ride-or-die-109831');
-// const data = await flixhq.fetchMediaServers('movie-109831');
-// const data = await flixhq.fetchMediaServers('episode-1019968');
-// const hianime = new HiAnime();
-// const data = await hianime.fetchSources('solo-leveling-18718-episode-119497', HiAnimeServers.HD2, SubOrDub.DUB);
-
-// console.log(JSON.stringify(data));
-// console.log(data.data?.sources);
 export { Anime, Meta, Anilist, Jikan, HiAnime, AnimeKai, FlixHQ, TheMovieDatabase, TvMaze };
--- todo.md
+++ todo.md
@@ -1,2 +1,17 @@
 // map episodes using string similairty
 '
+// import { _getsources } from './provider/movies/flixhq/flixhq.js';
+
+// const data = await _getsources('movie-109831', 'akcloud');
+// const data = await _getsources('episode-1019968', 'vidcloud');
+// const flixhq = new FlixHQ();
+//movie/final-destination-bloodlines-124888'
+// const data = await flixhq.fetchMediaInfo('tv/watch-the-boys-33895');
+// const data = await flixhq.fetchMediaInfo('movie/watch-bad-boys-ride-or-die-109831');
+// const data = await flixhq.fetchMediaServers('movie-109831');
+// const data = await flixhq.fetchMediaServers('episode-1019968');
+// const hianime = new HiAnime();
+// const data = await hianime.fetchSources('solo-leveling-18718-episode-119497', HiAnimeServers.HD2, SubOrDub.DUB);
+
+// console.log(JSON.stringify(data));
+// console.log(data.data?.sources);
