8eb23cd1b3345e1ebb59a2990bd65a716f3bad53
middle gear <otukojoshua@outlook.com>
2025-08-02 13:48:29 +0300

    chore(add linting and export classes): added linting and export classes

--- src/main.ts
+++ src/main.ts
@@ -31,10 +31,5 @@ export {
   StreamingServers,
   AnimeProvider,
 };
-// const flixhq = new FlixHQ();
-//movie/final-destination-bloodlines-124888'
-// const data = await flixhq.fetchSources('episode-1019968');
-const hianime = new HiAnime();
-const data = await hianime.fetchSources('solo-leveling-18718-episode-119497', HiAnimeServers.HD2, SubOrDub.DUB);
-console.log(JSON.stringify(data));
-// export { Anime, Meta, Anilist, Jikan, HiAnime, AnimeKai, FlixHQ, TheMovieDatabase, TvMaze };
+
+export { Anime, Meta, Anilist, Jikan, HiAnime, AnimeKai, FlixHQ, TheMovieDatabase, TvMaze };
